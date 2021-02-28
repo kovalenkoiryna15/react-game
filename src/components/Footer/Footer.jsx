f2f86185583058e6d0025aa86539fee6e77893a0
kovalenkoiryna15 <52598991+kovalenkoiryna15@users.noreply.github.com>
2021-02-28 18:42:37 +0300

    fix: author link ux

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,20 +6,12 @@ import GitHubLogoSVG from './GitHubLogoSVG';
 import RSSLogoSVG from './RSSLogoSVG';
 
 const Footer = () => (
-  <Row lg={12} md={12} sm={12} xs={12} className="footer text-centerrotate w-100">
+  <Row lg={12} md={12} sm={12} xs={12} className="footer text-centerrotate w-100 py-3">
     <Col md={4} sm={4} xs={12} className="footer-col">
       Battleship Game, 2021
     </Col>
     <Col md={4} sm={4} xs={12} className="footer-col">
-      <Row className="flex-grow-1">
-        <Col>
-          <a className="collaborator" href="https://github.com/kovalenkoiryna15" target="blank">
-            &copy; Iryna Kavalenka
-            &nbsp;
-            <GitHubLogoSVG />
-          </a>
-        </Col>
-      </Row>
+      <GitHubLogoSVG />
     </Col>
     <Col md={4} sm={4} xs={12} className="footer-col">
       <RSSLogoSVG />
